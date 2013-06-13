3bd6cf6bd76292e3de5ef49bcc51c8839147dabc
Michael Quinn <mquinn@gloto.com>
2013-06-13 14:47:51 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'gloto-cloud'
+maintainer       'Gloto Corp.'
+maintainer_email 'support@gloto.com'
+license          'All rights reserved'
+description      'Installs/Configures gloto-cloud'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
