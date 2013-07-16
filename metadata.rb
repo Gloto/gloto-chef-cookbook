c6529b074a99c51738b4eec3fb671b1e145f5d8a
Michael Quinn <mquinn@gloto.com>
2013-07-16 19:20:10 -0400

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "support@gloto.com"
 license          "All rights reserved"
 description      "Collection of recipes for setting up Gloto's servers"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+version          "0.1.1"
 recipe           "gloto-chef-cookbook", "Empty. Use another recipe."
 recipe           "gloto-chef-cookbook::apt-update", "Run apt-get update if it has been a while."
 recipe           "gloto-chef-cookbook::ldap-client", "Set up LDAP+PAM authentication"
