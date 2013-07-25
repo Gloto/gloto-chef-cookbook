03083cac48a674b6240a6844c5f3e9c1343f383b
Michael Quinn <mquinn@gloto.com>
2013-07-25 12:17:05 -0400

    Force apt-get update if the update-notifier-common package isn't installed

--- recipes/apt-update.rb
+++ recipes/apt-update.rb
@@ -1,3 +1,22 @@
+execute "apt-get-update" do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end
+
+# Vagrant installs don't have update-notifier-common installed
+execute "apt-get-update-initial" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    !File.exists?('/var/lib/apt/periodic/update-success-stamp')
+  end
+end
+
+package "update-notifier-common" do
+  notifies :run, resources(:execute => "apt-get-update"), :immediately
+end
+
 execute "apt-get-update-periodic" do
   command "apt-get update"
   ignore_failure true
